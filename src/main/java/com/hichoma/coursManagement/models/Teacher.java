156a464b55aac9e5ccc4414f040b40c88c1db5f6
HaithamBH <haitham.benhammouda@gmail.com>
2024-01-04 21:35:32 +0100

    add teacher model

---
+++ src/main/java/com/hichoma/coursManagement/models/Teacher.java
@@ -0,0 +1,31 @@
+package com.hichoma.coursManagement.models;
+
+public class Teacher {
+    private String ID;
+    private String Name;
+    private String Contact;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getContact() {
+        return Contact;
+    }
+
+    public void setContact(String contact) {
+        Contact = contact;
+    }
+}
